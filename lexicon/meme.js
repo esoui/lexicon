ae76c48494f684461528478fb74987eaf64ba33d
Arthur <arthur@corenzan.com>
2016-07-23 13:44:39 -0300

    Improve parameter matching

--- lexicon/meme.js
+++ lexicon/meme.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
 module.exports = {
-  pattern: '((create )?meme (.+?); ?(.+?); ?(.+)|(see )?memes( templates)?)',
+  pattern: '((create )?meme (.+)|(see )?memes( templates)?)',
   reply: function(message, match, reply) {
     if (match[3]) {
-      const params = [
-        encodeURIComponent(match[3]),
-        encodeURIComponent(match[4]),
-        encodeURIComponent(match[5])
-      ];
+      let params = match[3].split(';').map(function(value) {
+        return encodeURIComponent(value.replace(/^\s+|\s+$/, ''));
+      });
+      params[1] = params[1] || '_';
+      params[2] = params[2] || '_';
       reply(`http://memegen.link/${params.join('/')}.jpg`);
     } else {
       reply('See all [meme templates](http://memegen.link/templates).');
