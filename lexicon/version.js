4d181f3a73fd6779012600e7e731293d4161109a
Arthur <arthur@corenzan.com>
2016-07-23 03:23:22 -0300

    Accept both live and pts version

--- lexicon/version.js
+++ lexicon/version.js
@@ -1,9 +1,10 @@
 const https = require('https');
 
 module.exports = {
-  pattern: "(what('s| is) the )?current version",
+  pattern: "(what('s| is) the )?(current|live|pts) version",
   reply: function(message, match, reply) {
-    https.get('https://raw.githubusercontent.com/esoui/esoui/master/README.md', function(response) {
+    const branch = match[3] === 'pts' ? 'pts' : 'master';
+    https.get(`https://raw.githubusercontent.com/esoui/esoui/${branch}/README.md`, function(response) {
       response.setEncoding('utf-8');
       response.on('data', function(chunk) {
         const version = chunk.match(/Last update: (.+) on/);
