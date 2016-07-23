8cd51500bbd81bb20c5159d9e8c68641467e46b0
Arthur <arthur@corenzan.com>
2016-07-23 01:57:05 -0300

    Display help message on empty input

--- lexicon/help.js
+++ lexicon/help.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pattern: /help/,
+  pattern: /help|^$/,
   reply: function(message, match, reply) {
     reply("Here's what I know: \n- search add-ons ...\n- search author ...\n- search in forums ...\n- search wiki for ...\n- search source for ...\n- joke\n- current version\n- help");
   }
