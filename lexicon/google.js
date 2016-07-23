ebd5b4081a220b42c8cd9d5714549ca9e981b261
Arthur <arthur@corenzan.com>
2016-07-23 12:21:28 -0300

    Add Google search

---
+++ lexicon/google.js
@@ -0,0 +1,13 @@
+const querystring = require('querystring');
+
+module.exports = {
+  pattern: '(please )?google (.+)',
+  reply: function(message, match, reply) {
+    const params = {
+      q: match[2],
+    };
+    const site = match[1] ? 'http://lmgtfy.com/' : 'https://www.google.com/';
+    const uri = `${site}?${querystring.stringify(params)}`;
+    reply(`See [Google results for "${match[4]}"](${uri})`);
+  }
+};
