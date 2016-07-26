c8ac3b7ab73902b9548dd1070c5b28f05ec14994
Arthur <arthur@corenzan.com>
2016-07-26 01:12:10 -0300

    Update to ES6; Use faye instead of streaming

--- bot.js
+++ bot.js
@@ -1,28 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+import path from 'path';
+import fs from 'fs';
 
-const lexicon = [];
+const commands = [];
 {
-  const directory = path.join(__dirname, 'lexicon');
-  fs.readdirSync(directory).forEach(function(file) {
-    const entry = require(path.join(directory, file));
-    lexicon.push(entry);
+  const prefix = path.join(__dirname, 'commands');
+  fs.readdir(prefix, (err, files) => {
+    files.forEach((file) => {
+      const entry = require(path.join(prefix, file));
+      commands.push(entry);
+    });
   });
 }
 
 const help = "Hi, I'm the Lexicon, here's [what I can do](http://wiki.esoui.com/Lexicon).";
 
-const parse = function parse(message, reply) {
+export function parse(message, reply) {
   if (!message || !message.text) return;
   if (message.text.match(/^((lex(icon)?)( h[ea]lp)?|h[ea]lp)\s?$/)) return reply(help);
   let [ ,,text ] = message.text.match(/^lex(icon)?\s(.+)/) || [];
   if (!text) return;
 
-  lexicon.forEach(function(entry) {
-    if (!entry.pattern) return true;
-    const match = text.match(new RegExp('^' + entry.pattern, 'i'));
-    if (match) entry.reply(message, match, reply);
+  commands.forEach(function(command) {
+    if (!command.pattern) return true;
+    const match = text.match(new RegExp('^' + command.pattern, 'i'));
+    if (match) command.reply(message, match, reply);
   });
-};
+}
 
-module.exports = { parse };
--- index.js
+++ index.js
@@ -1,77 +1,119 @@
-const https = require('https');
-const querystring = require('querystring');
-const bot = require('./bot');
-const heartbeat = " \n";
+import Halley from 'halley';
+import https from 'https';
+import qs from 'querystring';
+import * as bot from './bot';
 
-function send(message) {
-  const payload = querystring.stringify({
-    text: message
-  });
+// -
+// -
+// -
 
+function sendChatMessage(text) {
+  const payload = JSON.stringify({ text });
   const options = {
     hostname: 'api.gitter.im',
     port: 443,
     path: `/v1/rooms/${process.env.GITTER_ROOM_ID}/chatMessages`,
     method: 'POST',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json',
       'Content-Length': Buffer.byteLength(payload),
       'Authorization': `Bearer ${process.env.GITTER_TOKEN}`
     }
   };
-
-  const request = https.request(options, function(response) {
-    response.on('data', function(chunk) {
-      const message = chunk.toString();
-      console.log(message);
-    });
-  });
-
-  request.on('error', function(e) {
-    console.error('Something went wrong', e.message);
-  });
-
-  request.write(payload);
-
-  request.end();
-}
-
-function reply(to, message) {
-  send(`@${to} ${message}`);
-}
-
-function listen() {
-  const options = {
-    hostname: 'stream.gitter.im',
-    port: 443,
-    path: `/v1/rooms/${process.env.GITTER_ROOM_ID}/chatMessages`,
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${process.env.GITTER_TOKEN}`
-    }
-  };
+  console.log('Send chat message', options);
 
   const request = https.request(options, function(response) {
     response.on('data', function(chunk) {
       response.setEncoding('utf-8');
-      if (chunk !== heartbeat) {
-        const message = JSON.parse(chunk);
-        if (message.error) {
-          console.error(message);
-        } else {
-          bot.parse(message, function(response) {
-            reply(message.fromUser.username, response);
-          });
-        }
-      }
+      const message = JSON.parse(chunk);
+      console.log('Send chat message', message);
     });
   });
 
-  request.on('error', function(e) {
-    console.error('Something went wrong', e.message);
-  });
-
+  request.on('error', (e) => console.error('Something went wrong', e.message));
+  request.write(payload);
   request.end();
 }
 
-listen();
+// -
+
+function updateEyeball(socketId, on) {
+  const payload = JSON.stringify({ socketId, on });
+  const options = {
+    hostname: 'api.gitter.im',
+    port: 443,
+    path: `/v1/eyeballs`,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(payload),
+      'Authorization': `Bearer ${process.env.GITTER_TOKEN}`
+    }
+  };
+  console.log('Set eyeball', options, payload);
+
+  const request = https.request(options, function(response) {
+    response.on('data', function(chunk) {
+      response.setEncoding('utf-8');
+      const message = JSON.parse(chunk);
+      console.log('Set eyeball', message);
+    });
+  });
+
+  request.on('error', (e) => console.error('Something went wrong', e.message));
+  request.write(payload);
+  request.end();
+}
+
+// -
+// -
+// -
+
+class AuthExt {
+  outgoing(message, callback) {
+    if (message.channel === '/meta/handshake') {
+      message.ext = message.ext || { token: process.env.GITTER_TOKEN };
+    }
+    callback(message);
+  }
+
+  incoming(message, callback) {
+    if (message.channel === '/meta/handshake') {
+      if (message.successful) {
+        console.log('Authorization granted', message);
+      } else {
+        console.error('Authorization denied', message);
+      }
+    }
+    callback(message);
+  }
+}
+
+// -
+// -
+// -
+
+const client = new Halley.Client('https://ws.gitter.im/bayeux');
+client.addExtension(new AuthExt());
+
+{
+  const uri = `/api/v1/rooms/${process.env.GITTER_ROOM_ID}`;
+  client.subscribe(uri).then(() => {
+    updateEyeball(client.getClientId(), true);
+    process.on('SIGINT', () => updateEyeball(client.getClientId(), false));
+  });
+}
+
+{
+  const uri = `/api/v1/rooms/${process.env.GITTER_ROOM_ID}/chatMessages`;
+  const handler = function(message) {
+    console.log('Chat message', message);
+
+    if (message.operation === 'create') {
+      bot.parse(message.model, (text) => {
+        sendChatMessage(`@${message.model.fromUser.username} ${text}`);
+      });
+    }
+  };
+  client.subscribe(uri, handler);
+}
