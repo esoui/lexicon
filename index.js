ebf614473b854f8d6be31368e7c90c9c2d7f9df0
Arthur <arthur@corenzan.com>
2017-01-23 22:24:28 -0200

    Also reply to private conversations #6

--- index.js
+++ index.js
@@ -7,13 +7,36 @@ function listenAndReply(roomId) {
   client.readChatMessages(roomId, (message) => {
     if (message.operation === 'create') {
       if (message.model.fromUser.id === process.env.GITTER_LEXICON_USER_ID) return;
-      if (!/^@?lex(icon)?/.test(message.model.text)) return;
+
+      let prefix = '';
+
+      if (roomId === process.env.GITTER_ESOUI_ROOM_ID) {
+        if (!/^@?lex(icon)?/.test(message.model.text)) return;
+        prefix += `@${message.model.fromUser.username} `;
+      }
 
       Lexicon.parse(message.model.text, (text) => {
-        client.sendChatMessage(roomId, `@${message.model.fromUser.username} ${text}`);
+        client.sendChatMessage(roomId, `${prefix}${text}`);
       });
     }
   });
 }
 
-listenAndReply(process.env.GITTER_ESOUI_ROOM_ID);
+const rooms = [];
+
+client.watchRoomUpdates(process.env.GITTER_LEXICON_USER_ID, (message) => {
+  const roomId = message.model.id;
+  if (message.operation === 'create' || message.operation === 'patch') {
+    if (message.operation === 'create') {
+      Lexicon.parse('', (text) => {
+        client.sendChatMessage(roomId, text);
+      });
+    }
+    if (rooms.indexOf(roomId) < 0) {
+      listenAndReply(roomId);
+      rooms.push(roomId);
+    }
+  }
+});
+
+// listenAndReply(process.env.GITTER_ESOUI_ROOM_ID);
