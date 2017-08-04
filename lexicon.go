057d1a4079e931a772e7b33dc3e02c2a4607f1dd
Arthur <arthur@corenzan.com>
2017-08-04 19:01:19 -0300

    Add goodbye/exit handler

--- lexicon.go
+++ lexicon.go
@@ -3,13 +3,21 @@ package main
 import (
 	"github.com/esoui/lexicon/bot"
 	"github.com/esoui/lexicon/bot/shell"
+	"os"
 )
 
 func main() {
 	a := shell.New()
 	b := bot.New(a)
+
 	b.Handle(`hi|hello`, func(msg *bot.Message) {
 		b.Reply(msg, "Hi there!")
 	})
+
+	b.Handle(`bye|exit`, func(msg *bot.Message) {
+		b.Reply(msg, "Goodbye!")
+		os.Exit(0)
+	})
+
 	b.Listen()
 }
