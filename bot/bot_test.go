26d9c7e3181887a5dcb5a5216d187627f3c41101
Arthur <arthur@corenzan.com>
2017-08-04 16:53:57 -0300

    Test Bot.Handle

--- bot/bot_test.go
+++ bot/bot_test.go
@@ -4,11 +4,21 @@ import "testing"
 
 func TestNew(t *testing.T) {
 	var i interface{} = New()
-    b, ok := i.(*Bot)
+	b, ok := i.(*Bot)
 	if !ok {
 		t.Error("New() didn't return a bot")
 	}
 	if b.Handlers == nil {
 		t.Error("New() didn't initialize handlers map")
+	}
+}
+
+func TestBotHandle(t *testing.T) {
+	b := New()
+	expr := `test`
+	handler := func(msg *Message) {}
+	b.Handle(expr, handler)
+	if len(b.Handlers) != 1 {
+		t.Error("Bot.Handle() didn't register new handler")
 	}
 }
