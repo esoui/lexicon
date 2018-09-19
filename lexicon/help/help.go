427cd851b88657114782bfa1c4ac98a20fcd890d
Arthur <arthur@corenzan.com>
2018-09-19 18:47:32 -0300

    Fix help message with something useful

--- lexicon/help/help.go
+++ lexicon/help/help.go
@@ -7,5 +7,5 @@ import (
 var Expr = `h[ea]lp|(.*)`
 
 func Handler(b *bot.Bot, m bot.Message, match []string) {
-	b.Reply(m, "What can I do you for?")
+	b.Reply(m, "Hey! Here's [what I can do for you](http://wiki.esoui.com/Lexicon).")
 }
