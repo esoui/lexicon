ed976ea62da1933ed6efdc5c3037692dc4448db5
Arthur <arthur@corenzan.com>
2023-02-04 22:34:23 -0300

    Finish initial implementation of search feature

--- src/features/search.js
+++ src/features/search.js
@@ -1,26 +1,68 @@
+function createGitHubSearchUrl(query) {
+  const url = new URL("https://github.com/search?type=code");
+  url.searchParams.set("q", `repo:esoui/esoui ${query}`);
+  return url.toString();
+}
+
+function createLuaSearchUrl(query) {
+  const url = new URL("http://devdocs.io/lua~5.1/");
+  url.hash = `#q=${query}`;
+  return url.toString();
+}
+
+function createWikiSearchUrl(query) {
+  const url = new URL("http://wiki.esoui.com/w/index.php?go=1");
+  url.searchParams.set("search", query);
+  return url.toString();
+}
+
+function createAddOnSearchUrl(query) {
+  const url = new URL("http://www.esoui.com/downloads/search.php");
+  url.searchParams.set("search", query);
+  return url.toString();
+}
+
+function createAuthorSearchUrl(query) {
+  const url = new URL("http://www.esoui.com/forums/memberlist.php?do=getall");
+  url.searchParams.set("ausername", query);
+  return url.toString();
+}
+
+function createEsouiSearchUrl(query) {
+  const url = new URL("http://www.esoui.com/forums/search.php?do=process");
+  url.searchParams.set("query", query);
+  return url.toString();
+}
+
 const corpus = {
   name: "Search Corpus",
   locale: "en-US",
   entities: {
     source: {
       options: {
+        source: ["source", "github", "ui", "interface"],
         addOn: ["add-on", "addon"],
+        author: ["author", "creator"],
         lua: ["Lua"],
-        api: ["API", "game API", "ESO API"],
+        wiki: ["wiki", "api"],
         esoui: ["ESOUI", "forum"],
-        forum: ["game forum", "ESO forum"],
       },
     },
     query: {
       trim: [
         {
           position: "between",
-          leftWords: ["for", "search"],
+          leftWords: ["for", "search", "find"],
           rightWords: ["in"],
         },
         {
           position: "afterFirst",
           words: ["for", "named"],
+        },
+        {
+          position: "afterLast",
+          words: ["search"],
+          opts: { noSpaces: true },
         },
       ],
     },
@@ -37,7 +79,7 @@ const corpus = {
         source: {
           mandatory: true,
           question:
-            "Where do you want to search? (add-ons, Lua, API, ESOUI or game forums)",
+            "Where do you want to search? (Lua documentation, interface source, add-ons, author, ESOUI wiki or ESOUI forums)",
         },
         query: {
           mandatory: true,
@@ -60,18 +102,37 @@ export default function (nlp) {
   nlp.registerActionFunction("handleSearch", async (data, locale) => {
     const source = data.entities.find(
       ({ entity }) => entity === "source"
-    )?.utteranceText;
+    )?.option;
     const query = data.entities.find(
       ({ entity }) => entity === "query"
     )?.utteranceText;
 
-    if (!source || !query) {
-      return data;
+    // Although entities are avaiable in answer interpolation,
+    // data.context.source = source;
+
+    if (source && query) {
+      switch (source) {
+        case "lua":
+          data.context.url = createLuaSearchUrl(query);
+          break;
+        case "wiki":
+          data.context.url = createWikiSearchUrl(query);
+          break;
+        case "source":
+          data.context.url = createGitHubSearchUrl(query);
+          break;
+        case "addOn":
+          data.context.url = createAddOnSearchUrl(query);
+          break;
+        case "author":
+          data.context.url = createAuthorSearchUrl(query);
+          break;
+        case "esoui":
+          data.context.url = createEsouiSearchUrl(query);
+          break;
+      }
     }
 
-    const url = new URL("https://google.com/search");
-    url.searchParams.set("q", query.trim());
-    data.context.url = url.toString();
     return data;
   });
 }
